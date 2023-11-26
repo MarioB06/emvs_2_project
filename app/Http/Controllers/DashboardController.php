20d1f6c3f9c38404b557276eaee4a15464ab8567
MarioB06 <mbozic2006@gmail.com>
2023-11-26 15:32:50 +0100

    routes for views in dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dashboard');
+    }
+
+    public function rank()
+    {
+        return view('dashboard.rank');
+    }
+
+    public function settings()
+    {
+        return view('dashboard.settings');
+    }
+
+    public function friends()
+    {
+        return view('dashboard.friends');
+    }
+
+    public function account()
+    {
+        return view('dashboard.account');
+    }
+
+    public function posts()
+    {
+        return view('dashboard.posts');
+    }
 }
