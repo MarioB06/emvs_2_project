6820ed5ed1bcb8bf94e88bd84e82fa4658428c05
MarioB06 <mbozic2006@gmail.com>
2023-11-28 14:58:54 +0100

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,15 +3,21 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
+        User::truncate();
 
+        User::create([
+            'username' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('123'),
+        ]);
     }
 }
