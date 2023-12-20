9dd6211b43a0602f031de20e8e387167beb15f3c
Luanasani <luanasani48@gmail.com>
2023-12-20 15:57:21 +0100

    Post System fast fertig

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,30 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\RankController;
 use Illuminate\Http\Request;
+use App\Models\Post;
+
 
 class PostController extends Controller
 {
     public function index()
     {
         $posts = Post::all();
-        return view('posts.index', ['posts' => $posts]);
+        return view('dashboard.posts', ['posts' => $posts]);
     }
 
     public function create()
     {
-        return view('posts.create');
+        return view('dashboard.posts');
     }
 
     public function store(Request $request)
     {
-       
+        
+        // Validiere die Eingabe
+        $request->validate([
+            'content' => 'required|string|max:255',
+            'title' => '',
+            
+            
+        ]);
 
+        // Erstelle den Beitrag in der Datenbank
         Post::create([
             'user_id' => auth()->id(),
             'content' => $request->input('content'),
+            'title' => 'Standardtitel',
+            'likes' => 0,
+            'dislikes' => 0,
         ]);
+        $userId = auth()->id();
+        $rankedController = new RankController();
+        $rankedController->post($userId);
 
-        return redirect()->route('posts.index');
+        // Optional: Zeige eine Bestätigungsnachricht oder leite den Benutzer weiter
+        return redirect()->route('dashboard.posts')->with('success', 'Beitrag erfolgreich erstellt.');
     }
 }
